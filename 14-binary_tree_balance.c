06b6a6904c26a03a8472efeb1cb4ec67fc1029de
Dennis Kimani <dxaldovah@gmail.com>
2023-08-29 12:40:26 -0700

    function that measures the balance factor of a binary tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,20 +1,23 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance - Measures the balance factor of a binary tree
- * @tree: Pointer to the root node of the tree to measure
- * Return: Balance factor of the tree, or 0 if tree is NULL
+ * binary_tree_is_full - Checks if a binary tree is full
+ * @tree: Pointer to the root node of the tree to check
+ * Return: 1 if tree is full, 0 otherwise
 */
-int binary_tree_balance(const binary_tree_t *tree)
+int binary_tree_is_full(const binary_tree_t *tree)
 {
-	int left_height = 0, right_height = 0;
-
 	if (tree == NULL)
 	{
 		return (0);
 	}
-	left_height = (int)binary_tree_height(tree->left);
-	right_height = (int)binary_tree_height(tree->right);
-
-	return (left_height - right_height);
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
+	if (tree->left != NULL && tree->right != NULL)
+	{
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	}
+	return (0);
 }
