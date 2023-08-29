4db698f8937e8468ffd31c9eb84c5180e2caa017
Dennis Kimani <dxaldovah@gmail.com>
2023-08-29 13:40:04 -0700

    fixes to files 14 and 16 and additions of testing dot c files

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,9 +7,6 @@
 */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_height = 0;
-	int right_height = 0;
-
 	if (tree == NULL)
 	{
 		return (0);
--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -7,12 +7,64 @@
 */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t height = binary_tree_height(tree);
-	size_t nodes = binary_tree_nodes(tree);
+	int left_height, right_height;
 
 	if (tree == NULL)
 	{
 		return (0);
 	}
-	return (nodes == (1 << height) - 1);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (left_height != right_height)
+	{
+		return (0);
+	}
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
+	return
+		(binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
+}
+
+/**
+ * binary_tree_measure - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure
+ * Return: Height of the tree, or 0 if tree is NULL
+*/
+size_t binary_tree_measure(const binary_tree_t *tree)
+{
+	size_t left_height = 0, right_height = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (0);
+	}
+	left_height = binary_tree_measure(tree->left) + 1;
+	right_height = binary_tree_measure(tree->right) + 1;
+
+	if (left_height > right_height)
+	{
+		return (left_height);
+	}
+	else
+	{
+		return (right_height);
+	}
+}
+
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree
+ * Return: The height of the tree
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	return (binary_tree_measure(tree));
 }
